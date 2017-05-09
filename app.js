8cdf0d1243c692a3a37b36740f770e9d6ec646aa
Matthew Spencer <matthew.spencer@sigma-systems.com>
2017-05-09 18:23:26 +0100

    Moved code into new project.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // DATA
-
 // var myActivity = {
 //     ID: 1,
 //     Name: "Work",
@@ -7,7 +6,6 @@
 //     End: "16:00",
 //     Color: "orange"
 // };
-
 // TODO: Create a function to loop through an array of activities and generate the UI dynamically
 // e.g.
 var myActivityList = [
@@ -26,33 +24,26 @@ var myActivityList = [
         Color: "green"
     }
 ];
-
-var activity =
-    {
-        ID: 1,
-        Name: "Work",
-        Start: "08:00",
-        End: "16:00",
-        Color: "orange"
-    };
-
+var activity = {
+    ID: 1,
+    Name: "Work",
+    Start: "08:00",
+    End: "16:00",
+    Color: "orange"
+};
 myActivityList.push(activity);
 myActivityList.pop();
-
 for (var i = 0; i < myActivityList.length; i++) {
     var index = i + 1;
     var id = "activity" + index;
     var acitivityDiv = document.getElementById(id);
-
     var nameInput = document.getElementById("name" + index);
     var startInput = document.getElementById("start" + index);
     var endInput = document.getElementById("end" + index);
-
     var myActivity = myActivityList[i];
-
     nameInput.value = myActivity.Name;
     startInput.value = myActivity.Start;
     endInput.value = myActivity.End;
-
     acitivityDiv.style.backgroundColor = myActivity.Color;
 }
+//# sourceMappingURL=app.js.map
